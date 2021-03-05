c995ab06d2a2b57cb8fccdfecda208843d8087fa
Marsel <60897982+MarselBurdo@users.noreply.github.com>
2021-03-05 22:21:24 +0300

    initial

---
+++ src/main/java/com/zdbank/newbie/NewbieApplication.java
@@ -0,0 +1,13 @@
+package com.zdbank.newbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zdbank/newbie/NewbieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdbank.newbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
