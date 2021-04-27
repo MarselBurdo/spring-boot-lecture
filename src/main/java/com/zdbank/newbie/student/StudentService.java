542258f29ec38bcdcc879fcb4ecbe43ff76fb2f8
Marsel <60897982+MarselBurdo@users.noreply.github.com>
2021-04-27 18:26:55 +0300

    post method done

--- src/main/java/com/zdbank/newbie/student/StudentService.java
+++ src/main/java/com/zdbank/newbie/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,6 +22,10 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentByEmail=studentRepository.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+       studentRepository.save(student);
     }
 };
